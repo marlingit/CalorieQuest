36bda49a60ad193912eadf185ef6b6e55ea647b9
Brighton Young <bnyg@vt.edu>
2024-05-01 22:32:10 -0400

    update

---
+++ CalorieQuest/CalorieQuest/Views/Search/Videos/VideosItem.swift
@@ -0,0 +1,19 @@
+//
+//  VideosItem.swift
+//  CalorieQuest
+//
+//  Created by Brighton Young on 5/1/24.
+//  Copyright © 2024 Marlin Spears. All rights reserved.
+//
+
+import SwiftUI
+
+struct VideosItem: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    VideosItem()
+}
---
+++ CalorieQuest/CalorieQuest/Views/Search/Videos/VideosList.swift
@@ -0,0 +1,19 @@
+//
+//  VideosList.swift
+//  CalorieQuest
+//
+//  Created by Brighton Young on 5/1/24.
+//  Copyright © 2024 Marlin Spears. All rights reserved.
+//
+
+import SwiftUI
+
+struct VideosList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    VideosList()
+}
